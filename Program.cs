5e655a46a1e79d3a24b5b83d48fc76aed6257ee2
Marzie Keshavarz <marziekeshavarz97@gmail.com>
2023-04-30 18:25:41 +0330

    Add existing project files to Git

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Parallel_Programmin_In_DotNet.Courses;
+
+namespace Parallel_Programmin_In_DotNet // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            CreatingAndStartingTasks.Start(args);
+
+        }
+    }
+}
